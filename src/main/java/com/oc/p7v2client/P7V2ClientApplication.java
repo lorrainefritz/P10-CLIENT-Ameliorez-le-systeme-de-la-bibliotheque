aca788e45706d83d90bd449efef563bc48e9389d
lorrainefritz <lorraine.fritz0@gmail.com>
2022-02-09 10:40:04 +0100

    First commit

---
+++ src/main/java/com/oc/p7v2client/P7V2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.oc.p7v2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P7V2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P7V2ClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oc/p7v2client/P7V2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.p7v2client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P7V2ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
