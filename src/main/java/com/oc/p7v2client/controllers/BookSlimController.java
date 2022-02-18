8006de6335b8c2ffc30b8f4bc84f472575220005
lorrainefritz <lorraine.fritz0@gmail.com>
2022-02-18 10:53:56 +0100

    Recherche par titre est ok

--- src/main/java/com/oc/p7v2client/controllers/BookSlimController.java
+++ src/main/java/com/oc/p7v2client/controllers/BookSlimController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,11 +20,52 @@ import java.util.List;
 public class BookSlimController {
     private final BookSlimProxy bookSlimProxy;
 
-    @GetMapping(value="/books")
-    public String listOfBooksSlims(Model model){
+    @GetMapping(value = "/books")
+    public String listOfBooksSlims(Model model) {
         log.info("HTTP GET request received at /books with listOfBooksSlims");
-        List<BookSlimBean> booksSlims = bookSlimProxy.bookSlimBeanList();
-        model.addAttribute("booksSlims", booksSlims);
-        return"booksList";
+            List<BookSlimBean> booksSlims = bookSlimProxy.bookSlimBeanList();
+            model.addAttribute("booksSlims", booksSlims);
+            model.addAttribute("keyword", new String());
+        return "booksList";
     }
+
+    @GetMapping(value = "/books/search")
+    public String listOfBooksSlimsWithKeyWord(Model model, @RequestParam(value="keyword") String keyword) {
+        log.info("HTTP GET request received at /books with listOfBooksSlimsWithKeyWord");
+        if (keyword == null) {
+            List<BookSlimBean> booksSlims = bookSlimProxy.bookSlimBeanList();
+            model.addAttribute("booksSlims", booksSlims);
+            model.addAttribute("keyword", new String());
+        } else {
+            log.info("HTTP GET request received at /books with listOfBooksSlimsWithKeyWord WITH keyword : " + keyword);
+            List<BookSlimBean> booksSlims = bookSlimProxy.booksSlimsListWithAKeyword(keyword);
+            model.addAttribute("booksSlims", booksSlims);
+            model.addAttribute("keyword", keyword);
+        }
+        return "booksList";
+    }
+
+
+    /*@GetMapping(value="/books/{keyword}")
+    public String listOfBooksSlims(Model model, @PathVariable String keyword){
+        log.info("HTTP GET request received at /books with listOfBooksSlims");
+       *//* if (keyword == null) {
+            List<BookSlimBean> booksSlims = bookSlimProxy.bookSlimBeanList();
+            model.addAttribute("booksSlims", booksSlims);
+            model.addAttribute("keyword", new String());
+        }else{*//*
+        log.info("HTTP GET request received at /books with listOfBooksSlims WITH keyword : " +keyword );
+        List<BookSlimBean> booksSlims = bookSlimProxy.booksSlimsListWithAKeyword(keyword);
+        model.addAttribute("booksSlims", booksSlims);
+        model.addAttribute("keyword", new String());
+        return"booksList";
+    }*/
+
+
+  /*  @PostMapping(value="/books/{keyword}")
+    public String searchForABooksSlimsListWithAKeyword(@ModelAttribute String keyword){
+        log.info("HTTP POST request received at /books/"+keyword+ "with searchForBooksSlimsList");
+        bookSlimProxy.searchForABooksSlimsListWithAKeyword(keyword);
+        return"redirect:/booksList";
+    }*/
 }
--- src/main/java/com/oc/p7v2client/proxies/BookSlimProxy.java
+++ src/main/java/com/oc/p7v2client/proxies/BookSlimProxy.java
@@ -3,11 +3,18 @@ package com.oc.p7v2client.proxies;
 import com.oc.p7v2client.beans.BookSlimBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @FeignClient(name="P7-V2-API",url="localhost:8083")
 public interface BookSlimProxy {
-    @GetMapping(value="/books")
+   @GetMapping(value="/books")
     public List<BookSlimBean> bookSlimBeanList();
+   @GetMapping(value="/books/search")
+   public List<BookSlimBean> booksSlimsListWithAKeyword(@RequestParam(value="keyword") String keyword);
+    /* @GetMapping(value="/books/{keyword}")
+    public List<BookSlimBean> booksSlimsListWithAKeyword(@PathVariable String keyword);*/
 }
